3ac5e6d6b51e27e6fa42a934352d5ae398f071e2
Khalil Hammami <khammami@gmail.com>
2024-02-24 18:49:38 +0100

    Fix aggregate script

--- scripts/aggregate_codelabs.sh
+++ scripts/aggregate_codelabs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Declare an empty array to store unique entries
-unique_entries=()
+set -euo pipefail  # Enable strict error handling
 
 # Iterate through each subfolder within the codelabs folder
 for folder in codelabs/*/; do
@@ -11,18 +10,17 @@ for folder in codelabs/*/; do
     source=$(jq -r '.source' "$folder/codelab.json")
     id=$(jq -r '.id' "$folder/codelab.json")
 
-    # Skip entry if it already exists in the new codelabs.json
-    entry="{\"source\": \"$source\", \"id\": \"$id\"}"
-    if ! grep -q "$source" "codelabs.json"; then
-      # Add the extracted fields as a new entry to the array
-      unique_entries+=("$entry")
+    filename="codelabs.json"
+    entry="{\"id\": \"$id\", \"source\": \"$source\"}"
+
+    # Create the file with an empty array if it doesn't exist
+    [ -f "$filename" ] || echo '[]' > "$filename"
+
+    # Check for existing entry based on "source" key using jq
+    if ! jq -r 'any(.source == $source)' "$filename"; then
+      jq -r ". += [$entry]" "$filename" > "$filename.tmp"
+      mv "$filename.tmp" "$filename"
+      echo "Successfully added entry with id: $id and source: $source"
     fi
   fi
 done
-
-# Create the codelabs.json file with the unique entries
-cat > codelabs.json <<EOF
-[
-    ${unique_entries[*]}
-]
-EOF
